201ae9a043985cb65650f835acba7a76105b5bcd
Si Le <omnisyle@gmail.com>
2020-04-14 22:47:55 -0400

    adjust spacings for smaller height screens

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -3,24 +3,32 @@ import classNames from "classnames";
 
 import styles from "./styles.module.scss";
 
-
 interface PropTypes {
   position: "center" | "left";
+  size: "sm" | "md"
 }
 
 const defaultProps = {
   position: "left",
+  size: "sm",
 };
 
 const Logo = (props: PropTypes) => (
   <header
     className={
       classNames(
-        { [styles.center]: props.position === "center" }
+        {
+          [styles.center]: props.position === "center" ,
+        }
       )
     }
   >
-    <h1 className={styles.logo}>Pointers.</h1>
+    <h1 className={classNames(styles.logo, {
+      [styles.md]: props.size === "md",
+      [styles.sm]: props.size === "sm"
+    })}>
+      Pointers.
+    </h1>
   </header>
 );
 
