fddf5cce443227377615cfeb398124f54c3477a3
Si Le <omnisyle@gmail.com>
2020-04-19 00:53:51 -0400

    changes join page to match with invite page

--- src/pages/join/index.tsx
+++ src/pages/join/index.tsx
@@ -1,55 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
-import Logo from "../../components/logo";
-import styles from "./styles.module.scss";
+import JoinUI from "./ui";
 
 const Join = () => {
-  const [name, setName] = useState("");
-
-  const onNameChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value);
-  };
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-  };
-
   return (
-    <div className={styles.join}>
-      <Logo position="center"/>
-
-      <div className={styles.content}>
-        <p className={styles.joinText}>
-          You are invited to join <span className={styles.userName}>Amy's</span> room
-        </p>
-
-        <img
-          className={styles.hero}
-          src="https://firebasestorage.googleapis.com/v0/b/pointer-3906a.appspot.com/o/public%2Fimages%2Fprofile-cat.png?alt=media"
-          alt="profile-cat"
-        />
-
-        <p className={styles.tagLine}>
-          A (nick)name will help your fur-ends recognize you
-        </p>
-
-        <form id={styles.roomForm} onSubmit={onSubmit}>
-          <input
-            name="name"
-            value={name}
-            onChange={onNameChanged}
-            type="text"
-            placeholder="Your name, please"
-          />
-
-          <input
-            type="submit"
-            className="btn btn-success"
-            value="Join room"
-          />
-        </form>
-      </div>
-    </div>
+    <JoinUI />
   );
 };
 
---
+++ src/pages/join/interactor.ts
@@ -0,0 +1,7 @@
+const joinSession = () => {
+
+};
+
+export default {
+  joinSession
+};
